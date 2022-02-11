9744b04ef9b84b4b602bad816330258208d3e92f
Deki97 <dejan97@libero.it>
2022-02-11 17:08:03 +0100

    done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,30 @@
-<h1>Homepage</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Homepage</h1>
 
-<h2>Lista dei film</h2>
+    <h2>Films</h2>
 
-@foreach ($movies as $movie)
-    <div>
-        {{ $movie->title }}
-    </div>
-@endforeach
+    @foreach ($movies as $movie)
+        <div>
+            Title: {{ $movie->title }}
+        </div>
+
+        <div>
+            Original Title: {{ $movie->original_title }}
+        </div>
+
+        <div>
+            Vote: {{ $movie->vote }}
+        </div>
+    @endforeach
+
+</body>
+</html>
