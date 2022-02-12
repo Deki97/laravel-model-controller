c14f7f5d28d33b0aac4024432318e57eafec62bb
Deki97 <dejan97@libero.it>
2022-02-12 09:58:24 +0100

    added some styling css movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,40 @@
 @extends('layouts.app')
 
-<h1>Homepage</h1>
+<div class="container">
+    <h1>Homepage</h1>
 
-<h2>Films</h2>
+    <h2>Films</h2>
 
-@foreach ($movies as $movie)
-    <div>
-        Title: {{ $movie->title }}
+    <div class="movies">
+        @foreach ($movies as $movie)
+        <div class="single-movie">
+            <div class="title">
+                <span class="tag">
+                    Titolo
+                </span> 
+                <div>
+                    {{ $movie->title }}
+                </div>
+            </div>
+        
+            <div class="original-title">
+                <span class="tag">
+                    Titolo originale
+                </span> 
+                <div>
+                    {{ $movie->original_title }}
+                </div>
+            </div>
+        
+            <div class="vote">
+                <span class="tag">
+                    Voto
+                </span> 
+                <div>
+                    {{ $movie->vote }}
+                </div>
+            </div>
+        </div>
+        @endforeach
     </div>
-
-    <div>
-        Original Title: {{ $movie->original_title }}
-    </div>
-
-    <div>
-        Vote: {{ $movie->vote }}
-    </div>
-@endforeach
+</div>
