bc920d27119610b509ad5adaa5f971d7a3d3e81c
Deki97 <dejan97@libero.it>
2022-02-12 18:25:11 +0100

    added comments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,8 @@
+{{-- File home.blade.php che contiene la views della schermata home, ovvero quella di default --}}
+{{-- Questa è la pagina che viene mostrata appena il progetto viene caricato --}}
+{{-- Infatti nella route web.php, è richiamata questa pagina, dato che è stata passata alla view tramite il Controller --}}
+
+{{-- Indico che estende il layout di base che ho definito all'interno del file app.blade.php --}}
 @extends('layouts.app')
 
 <div class="container">
@@ -6,6 +11,7 @@
     <h2>Films</h2>
 
     <div class="movies">
+        {{-- $movies è il nome della variabile nella quale in HomeController ho preso tutti i dati dal database tramite il Model Movie --}}
         @foreach ($movies as $movie)
         <div class="single-movie">
             <div class="title">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Importo un font personalizzato da Google, ovvero in questo caso Roboto --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    {{-- Creo il link al file css, che da scss verrà tradotto in css tramite il comando da terminale npm install --}}
+    {{-- Con npm install importo le librerie del front-end, tra cui il css tradotto da scss --}}
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
-    
+    {{-- Questo è il file che contiene tutta la base del mio layout, dove importo gli stili ecc.. --}}
+    {{-- Qui posso includere delle partials come header, footer oppure definire tramite yield delle partials che saranno diverse per ogni pagina, per esempio il main --}}
+    {{-- Ad esempio posso definire uno @yield nel main dato che il suo contenuto potrebbe cambiare a seconda di in quale pagina mi trovo --}}
 </body>
 </html>
